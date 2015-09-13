889518068ef6e2faecc4bf36412e87115e7d0f2c
Zach Pace <zpace21@gmail.com>
2015-09-13 15:40:17 -0500

    more progress towards fitting

--- MaNGA_stars_gas_fit.py
+++ MaNGA_stars_gas_fit.py
@@ -6,6 +6,7 @@ import astropy.table as table
 
 from astropy import units as u, constants as c
 import ppxf_util as util
+from ppxf import ppxf
 from scipy import ndimage
 import manga_tools as m
 
@@ -152,7 +153,7 @@ def setup_MaNGA_stellar_libraries(fname_ifu, fname_tem, plot=False):
             (FWHM_avg_s / (1. + z))**2. - FWHM_tem**2.)
         sigma = FWHM_diff/2.355/dL_tem
 
-        a_f = convolve_variable_width(tems.data, sigma, prec=2.)
+        a_f = convolve_variable_width(tems.data, sigma, prec=3.)
 
         fname2 = 'stellar_libraries/st-{0:.4f}.fits'.format(z)
         print '\tMaking HDU:', fname2
@@ -161,3 +162,141 @@ def setup_MaNGA_stellar_libraries(fname_ifu, fname_tem, plot=False):
         blurred_hdu.header = tems.header
         blurred_hdu.header['z'] = z
         blurred_hdu.writeto(fname2, clobber=True)
+
+
+def ppxf_run_MaNGA_galaxy(ifu, fname_tem):
+    plate = ifu[0]
+    ifudsgn = ifu[1]
+    fname_ifu = 'manga-{}-{}-LOGCUBE.fits.gz'.format(plate, ifudsgn)
+
+    # first read in templates
+    # templates have already been convolved to the proper resolution
+    tems = fits.open(fname_tem)
+    htems = tems[0].header
+    dtems = tems[0].data
+
+    logL_tem = np.linspace(
+        htems['CRVAL3'],
+        htems['CRVAL3'] + dtems.shape[2]*htems['CDELT3'],
+        dtems.shape[2])  # base e
+
+    L_tem = np.exp(logL_tem)
+
+    dtems_med = np.median(dtems)
+    dtems /= dtems_med
+
+    # now read in IFU
+    ifu = fits.open(fname_ifu)
+    ifu_flux = ifu['FLUX'].data
+    ifu_ivar = ifu['ivar'].data
+    ifu_mask = ifu['MASK'].data
+    L_ifu = ifu['WAVE'].data
+    logL_ifu = np.log(L_ifu)
+
+    # now read in drpall
+    drpall = table.Table.read(m.drpall_loc + 'drpall-v1_3_3.fits',
+                              format='fits')
+    objconds = drpall['plateifu'] == '{}-{}'.format(plate, ifudsgn)
+    obj = drpall[objconds]
+
+    print obj['plateifu', 'nsa_redshift', 'nsa_vdisp', 'ebvgal']
+
+    c = 299792.458
+    dl = L_tem[0] - L_ifu[0]
+    dv = c * (logL_tem[0] - logL_ifu[0])  # km/s
+    vel = obj['nsa_redshift'] * c
+    veldisp = obj['nsa_vdisp']
+    if veldisp < 0.:  # deal with non-measured veldisps
+        veldisp = 300.
+
+    velscale = (logL_ifu[1] - logL_ifu[0])*c
+    moments = 4
+    regul_err = .004
+    ebvgal = obj['ebvgal']
+    dv = c*(logL_tem[0] - logL_ifu[0])
+
+    start = [vel, veldisp]
+
+    ifu_fits = np.nan * np.ones(ifu_flux.shape[:-1])
+
+    spaxels = which_spaxels(fname_ifu)
+
+    for spaxel in spaxels:
+        if spaxel['good'] == True:
+            gridx, gridy = spaxel['gridx'], spaxel['gridy']
+            goodpixels = (ifu_mask[:, gridx, gridy] & 10)
+            goodpixels *= (ifu_mask[:, gridx, gridy] & 8)
+
+            galaxy = ifu_flux[:, gridx, gridy]
+            med = np.median(galaxy)
+
+            pp = ppxf(templates=dtems,
+                      galaxy=galaxy/med,
+                      noise=1./np.sqrt(ifu_ivar[:, gridx, gridy]),
+                      goodpixels=goodpixels, vsyst=dv,
+                      velScale=velscale, moments=moments, degree=-1,
+                      mdegree=-1, regul=1./regul_err, reddening=ebvgal,
+                      clean=False, lam=L_ifu)
+            ifu_fits[spaxel[gridx, gridy]] = pp
+            break
+
+
+def which_spaxels(fname_ifu):
+    '''
+    list of spaxel indices in the order that they should be run
+    '''
+
+    ifu = fits.open(fname_ifu)
+    ifu_flux = ifu['FLUX'].data
+    r_im = ifu['RIMG'].data
+
+    # now figure out where in the ifu the center of the galaxy is
+    # and use that info to figure out where in the galaxy to start
+
+    NL, NX, NY = ifu_flux.shape
+    # print NX
+    # print pixpos_x.shape, pixpos_y.shape, r_im.shape
+
+    pos_x = np.linspace(-0.5*NX/2., 0.5*NX/2., NX)
+    pos_y = np.linspace(-0.5*NY/2., 0.5*NY/2., NY)
+    pixpos_x, pixpos_y = np.meshgrid(pos_x, pos_y)
+
+    peak_inds = np.unravel_index(np.argmax(r_im), pixpos_x.shape)
+    # print peak_inds
+
+    peak_distance = np.sqrt((pixpos_x - pos_x[peak_inds[1]])**2. +
+                            (pixpos_y - pos_y[peak_inds[0]])**2.)
+
+    grid = np.indices(peak_distance.shape)
+    gridx, gridy = grid[1], grid[0]
+
+    xnew = pixpos_x - peak_inds[1]
+    ynew = pixpos_y - peak_inds[0]
+
+    theta_new = np.arctan2(ynew, xnew)
+
+    # peak_distance and theta_new are coords relative to ctr of galaxy
+
+    # plt.imshow(r_im, origin='lower', aspect='equal')
+    # plt.scatter(peak_inds[1], peak_inds[0])
+    # plt.show()
+
+    # now make a table out of everything, so that it can get sorted
+
+    pixels = table.Table()
+    pixels['good'] = m.good_spaxels(ifu).flatten()
+    pixels['gridx'] = gridx.flatten()
+    pixels['gridy'] = gridy.flatten()
+    pixels['r'] = peak_distance.flatten()
+    pixels['theta'] = theta_new.flatten()
+    pixels.sort(['r', 'theta'])
+    pixels['order'] = range(len(pixels))
+
+    print pixels
+
+    '''plt.scatter(pixels['gridx'], pixels['gridy'],
+                c=pixels['order']*pixels['good'], s=5)
+    plt.colorbar()
+    plt.show()'''
+
+    return pixels
